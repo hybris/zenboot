8fbea22bea8aa46b94e7e1074f96744b85ff0b15
Johannes Engelke <johannes.engelke@hybris.com>
2013-10-15 16:25:40 +0200

    Fixed small URL Mapping bug

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -17,9 +17,8 @@ class UrlMappings {
         //REST
         "/rest/customer/$id/$property?"(controller:'customer', action:'rest')
         "/rest/action/$id/status"(controller:'executionZoneAction', action:'rest')
-        "/rest/hiera/$puppetEnvironment"(controller:'hieraRest', action:'rest')
-        "/rest/hiera/$puppetEnvironment/$qualityStage"(controller:'hieraRest', action:'rest')
-        "/rest/properties/$puppetEnvironment"(controller:'propertiesRest', action:'rest')
+        "/rest/hiera/$puppetEnvironment/$qualityStage?"(controller:'hieraRest', action:'rest')
+        "/rest/properties/$puppetEnvironment/$qualityStage?"(controller:'propertiesRest', action:'rest')
         "/rest/$url?"(controller:'exposedExecutionZoneAction', action:'rest')
         
         "500"(view:'/error')
