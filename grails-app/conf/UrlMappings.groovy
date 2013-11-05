1924f4b744650d3b3f6c795cb04217973ec164d5
Johannes Engelke <johannes.engelke@hybris.com>
2013-11-05 16:52:38 +0100

    TA7370 Changed mapping to fit templates to rest convention

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,7 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-        
+
         
         //homepage
         "/"(controller:'home', action:'index')
@@ -16,12 +16,12 @@ class UrlMappings {
         "/rest/customer/$id/$property?"(controller:'customer', action:'rest')
         "/rest/action/$id/status"(controller:'executionZoneAction', action:'rest')
         "/rest/hiera/$puppetEnvironment/$qualityStage?"(controller:'hieraRest', action:'rest')
-        
+        "/rest/templates" (resources:'template')
         "/rest/properties/$puppetEnvironment/$qualityStage?"(controller:'propertiesRest', action:'rest')
         "/rest/$url?"(controller:'exposedExecutionZoneAction', action:'rest')
         
         // templates
-        "/executionZone/$execId/template/$action?/$id?"(controller:'template')
+        name template : "/template/$action?/$id?" { controller='template' }
         "/properties/$id"(controller:'propertiesRest', action:'showFile')
         
         "500"(view:'/error')
