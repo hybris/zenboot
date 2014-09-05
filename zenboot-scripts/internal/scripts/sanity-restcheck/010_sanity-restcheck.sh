411d795932d4bc4841e611059141eeff86f58240
Kim Neunert <kim.neunert@hybris.com>
2014-09-06 00:39:29 +0200

    trying to get the sanitychecks green

--- zenboot-scripts/internal/scripts/sanity-restcheck/010_sanity-restcheck.sh
+++ zenboot-scripts/internal/scripts/sanity-restcheck/010_sanity-restcheck.sh
@@ -13,8 +13,14 @@ assert_http_code $URL 201 "POST" "Content-Type:text/xml" "sanitycheck:sanitychec
 
 echo "# Make a second call " 
 TMPFILE=`tempfile`
-CALLBACK=`curl -sL  --write-out '%{http_code}' --request POST --max-time 5 --basic --user 'sanitycheck:sanitycheck' -H 'Content-Type:text/xml' http://127.0.0.1:8080/zenboot/rest/sanitycheck | sed "s/.*<referral>//" | sed "s/<\/referral>.*//"`
+RETURNVALUE=`curl -sL  --write-out '%{http_code}' --request POST --max-time 5 --basic --user 'sanitycheck:sanitycheck' -H 'Content-Type:text/xml' http://127.0.0.1:8080/zenboot/rest/sanitycheck`
+echo "RETURNVALUE:-----------------------------------------------------"
+echo $RETURNVALUE
+echo "END--------------------------------------------------------------"
+
+CALLBACK=`echo $RETURNVALUE | sed "s/.*<referral>//" | sed "s/<\/referral>.*//"`
 echo "# CALLBACK is $CALLBACK"
+sleep 1
 assert_http_response $CALLBACK "RUNNING" "-H 'Content-Type:text/xml' --user sanitycheck:sanitycheck"
 sleep 8
 assert_http_response $CALLBACK "SUCCESS" "-H 'Content-Type:text/xml' --user sanitycheck:sanitycheck"
