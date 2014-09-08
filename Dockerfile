69fe37d286bb63505f627ce6289a3ce472065902
Kim Neunert <k9ert@gmx.de>
2014-09-08 16:32:07 +0200

    fixing docker-image

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN sudo apt-get update && sudo apt-get install -y curl ansible openssh-client s
 ADD docker-provisioning/ansible.cfg /etc/ansible/ansible.cfg
 RUN mkdir -p /home/user/zenboot
 ADD https://github.com/hybris/zenboot/releases/download/v0.7.8/zenboot.war /home/user/tomcat7/webapps/zenboot.war
+RUN sudo chown user:user /home/user/tomcat7/webapps/zenboot.war
 
 EXPOSE 8080
--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,13 @@
 #/bin/bash
 
 CWD=`pwd`
-sudo docker run -t -p 8080:8080 -v ${CWD}:/home/user/zenboot k9ert/zenboot
+dockerimage="k9ert/zenboot"
+
+echo -n "type i[enter] for interactive (need to run catalina.sh start manually) [ ] "
+read interactive
+
+if [ $interactive == "i" ] ; then
+sudo docker run -t -i -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage /bin/bash
+else
+sudo docker run -t -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage
+fi 
