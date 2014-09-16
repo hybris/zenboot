ba4bf6970a38732330d71ae1f21d9604c1ecc0b9
Kim Neunert <k9ert@gmx.de>
2014-09-16 14:08:17 +0200

    issue-22 mysql-connection

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,6 +7,16 @@ dataSource {
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
+
+    //run the evictor every 30 minutes and evict any connections older than 30 minutes.
+    minEvictableIdleTimeMillis=1800000
+    timeBetweenEvictionRunsMillis=1800000
+    numTestsPerEvictionRun=3
+    //test the connection while its idle, before borrow and return it
+    testOnBorrow=true
+    testWhileIdle=true
+    testOnReturn=true
+    validationQuery="SELECT 1"
 }
 
 hibernate {
