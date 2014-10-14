8e15e0880b20dae88f3ce33df50eef720df7be5b
Kim Neunert <kim.neunert@hybris.com>
2014-10-14 20:47:41 +0200

    fix the need to sudo in the scripts

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,24 @@
 #/bin/bash
 
-CWD=`pwd`
 dockerimage="k9ert/zenboot"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -i)
+      dockerimage=$2
+      shift 2 ;;
+  esac
+done
+
+CWD=`pwd`
 
 echo -n "type i[enter] for interactive (need to run catalina.sh start manually) [ ] "
 read interactive
 
+#docker run --privileged=true -e DEV_UID=$UID -e DEV_GID=$GID -i -t -v ${HOME}/devel:/devel willb/java-dev:centos7-spark-uid
+
 if [ $interactive == "i" ] ; then
-sudo docker run -t -i -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage /bin/bash
+sudo docker run -e DEV_UID=$UID -e DEV_GID=$GID -t -i -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage /bin/bash
 else
-sudo docker run -t -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage
-fi 
+sudo docker run -e DEV_UID=$UID -e DEV_GID=$GID -t -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage
+fi
