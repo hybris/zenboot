83afdfb43568a4e12c90e0da8ae5fa6d58f02ae5
Kim Neunert <k9ert@gmx.de>
2015-01-22 16:22:27 +0100

    adding jmx-capability

--- docker-provisioning/setenv.sh
+++ docker-provisioning/setenv.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=512m"
+
+hostname=`grep java.rmi.server.hostname /home/user/zenboot/zenboot.Docker.properties | cut -d'=' -f2`
+
+export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=8090 "
+
+if [ "AA$hostname" != "AA" ]; then
+  export CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=${hostname}"
+fi
--- docker-run.sh
+++ docker-run.sh
@@ -3,11 +3,13 @@
 CWD=`pwd`
 dockerimage="k9ert/zenboot"
 
+[ -f zenboot.Docker.properties ] || cp zenboot.Docker.properties.template zenboot.Docker.properties
+
 echo -n "type i[enter] for interactive (need to run catalina.sh start manually) [ ] "
 read interactive
 
 if [ $interactive == "i" ] ; then
-sudo docker run -t -i -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage /bin/bash
+sudo docker run -t -i -p 8080:8080 -p 8090:8090 -v ${CWD}:/home/user/zenboot $dockerimage /bin/bash
 else
-sudo docker run -t -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage
-fi 
+sudo docker run -t -p 8080:8080 -p 8090:8090 -v ${CWD}:/home/user/zenboot $dockerimage
+fi
