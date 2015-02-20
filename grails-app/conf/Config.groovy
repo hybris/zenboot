b3082ac8eb77c15b818810555aac04fdb220c9c7
Kim Neunert <k9ert@gmx.de>
2015-02-20 11:51:38 +0100

    open up some stuff to ROLE_USER

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -156,6 +156,9 @@ grails.plugins.springsecurity.controllerAnnotations.staticRules = [
     '/exposedExecutionZoneAction/show':     [Role.ROLE_USER],
     '/exposedExecutionZoneAction/execute':  [Role.ROLE_USER],
     '/scriptletBatch/ajaxList':             [Role.ROLE_USER],
+    '/host/list':                           [Role.ROLE_USER],
+    '/host/show':                           [Role.ROLE_USER],
+    '/scriptletBatch/list':                 [Role.ROLE_USER],
     //default
     '/**':                                  [Role.ROLE_ADMIN],
  ]
