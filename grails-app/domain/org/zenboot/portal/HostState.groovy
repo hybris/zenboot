cd4e967366354f31b16c9cd691f91790ff274bc9
Kim Neunert <kim.neunert@hybris.com>
2015-07-23 21:43:40 +0200

    added HostState REQUESTING

--- grails-app/domain/org/zenboot/portal/HostState.groovy
+++ grails-app/domain/org/zenboot/portal/HostState.groovy
@@ -3,6 +3,7 @@ package org.zenboot.portal
 enum HostState {
     //ATTENTION: order is used in comparisons to identify a lower or advances state! Don't change this order except you have a valid reason.
     UNKNOWN,
+    REQUESTING,
     CREATED,
     ACCESSIBLE,
     COMPLETED,
