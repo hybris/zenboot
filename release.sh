c2f7e44960b792f5d3b9077f4743d4ddd826e9e2
Martin Sander <martin.sander@sap.com>
2016-02-10 18:00:05 +0100

    prevent releases from master and if master is not up to date

--- release.sh
+++ release.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 set -e
+FORCE=false
 
 while [ $# -gt 0 ]; do
   case "$1" in
     -v)
       VERSION=$2
       shift 2 ;;
+    -f)
+      FORCE=true
+      shift 1 ;;
   esac
 done
+
+if [[ $(git name-rev --name-only HEAD) != master ]]; then
+    echo refusing to release from a branch that is not master
+    exit 1
+fi
+
+upstream=$(git for-each-ref --format='%(upstream:short)' refs/heads/master)
+if [[ $FORCE != true ]] && ! git diff --quiet --exit-code $upstream; then
+    echo "you are not on the latest commit of you upstream $upstream"
+    echo "use -f to force the release on your current HEAD"
+    exit 1
+fi
 
 [ -z "${VERSION:+x}" ]    && echo "# Error: VERSION not present or empty" && exit 2
 # version is not allowed to start with a letter
@@ -16,7 +32,7 @@ echo $VERSION | egrep -v -q "^[0-9]" && echo "# VERSION needs to start with a di
 
 # Check whether travis finished the build, something like:
 while [ ! "`curl https://api.travis-ci.org/repos/hybris/zenboot/builds | jq -r .[0].state`XX" == "finishedXX" ]; do
-  echo "# build is not finished ... waiting ..."
+  echo "# last travis build is not finished ... waiting ..."
   sleep 20
 done
 
