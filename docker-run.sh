1a5d9f7dd93dd2ec71b3b17e33a7ddac448ded8c
Daniel Steiert <danie.steiert@sap.com>
2016-09-13 13:49:34 +0200

    Refactor docker-run script to be easier to read and use

--- docker-run.sh
+++ docker-run.sh
@@ -2,16 +2,17 @@
 
 CWD=`pwd`
 dockerimage="hybris/zenboot"
+PARAMETERS="-p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage"
 
 [ -f zenboot.Docker.properties ] || cp zenboot.Docker.properties.template zenboot.Docker.properties
 
 if [[ -z $interactive ]]; then
-    echo -n "type i[enter] for interactive (need to run catalina.sh start manually) [ ] "
+    echo -n "type i[enter] for interactive (need to run catalina.sh start manually) [n] "
     read interactive
 fi
 
-if [ $interactive == "i" ] ; then
-    docker run -t -i -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage /bin/bash
+if [[ -z $interactive && $interactive == "i" ]] ; then
+    docker run -t -i ${PARAMETERS} /bin/bash
 else
-    docker run -t -p 8080:8080 -v ${CWD}:/home/user/zenboot $dockerimage
+    docker run -t -d ${PARAMETERS}
 fi
