5008ec84458e36f71975132bbb6894fe224972ee
Daniel Steiert <danie.steiert@sap.com>
2016-09-26 19:03:49 +0200

    Change hostname grepping to only get one IP and ignore comments

--- docker-provisioning/setenv.sh
+++ docker-provisioning/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=512m"
 
-hostname=`grep java.rmi.server.hostname /home/user/zenboot/zenboot.Docker.properties | cut -d'=' -f2`
+hostname=`cat /home/user/zenboot/zenboot.Docker.properties | grep -v # | grep java.rmi.server.hostname | cut -d'=' -f2 | sed ':a;N;$!ba;s/\n.*//g'`
 
 export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=8090 "
 
