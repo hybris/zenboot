3be613d1ec0f9d94a241d08941f1cacee8528589
Steiert, Daniel <daniel.steiert@sap.com>
2016-11-07 14:33:46 +0000

    Remove MaxPermSize parameter, since it is deprecated since JDK 8

--- docker-provisioning/setenv.sh
+++ docker-provisioning/setenv.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=512m"
-
 hostname=`cat /home/user/zenboot/zenboot.Docker.properties | grep -v '#' | grep java.rmi.server.hostname | cut -d'=' -f2 | sed ':a;N;$!ba;s/\n.*//g'`
 
 export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=8090 "
