7f5d19fbc77a7396240fa8e126a42e28f4506424
Luca Popesco <luca.popesco@sap.com>
2017-03-28 13:12:05 -0400

    RPI-2226 fix permissions error caused by disallowed parametrs wrapping in textbox

--- grails-app/controllers/org/zenboot/portal/processing/ExecutionZoneController.groovy
+++ grails-app/controllers/org/zenboot/portal/processing/ExecutionZoneController.groovy
@@ -367,7 +367,7 @@ class ExecutionZoneController extends AbstractRestController implements Applicat
     boolean unallowedZoneParameterEdit(parameter, originalParameter) {
       // multiline-support therefore replace newlines before comparison
         originalParameter?.description != parameter?.description ||
-                (originalParameter?.value?.replaceAll("[\\\t|\\\n|\\\r]","") != parameter?.value &&
+                (originalParameter?.value?.replaceAll("[\\\t|\\\n|\\\r]","") != parameter?.value.replaceAll("[\\\t|\\\n|\\\r]","")  &&
                         !executionZoneService.canEdit(springSecurityService.currentUser.getAuthorities(), parameter))
     }
 
