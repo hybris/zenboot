d1a448ae1df4329c8a1d3e9b247bad68e51933ca
Michael Stadelmeier <michael.stadelmeier@hybris.com>
2017-08-01 16:02:28 +0200

    If the script directory / file not exists an exception will thrown and the action still remains in the process queue as waiting. This changes will set the state of the ScriptletBatch to failure and shows the error message in the view for this action.

--- grails-app/services/org/zenboot/portal/processing/ScriptletBatchService.groovy
+++ grails-app/services/org/zenboot/portal/processing/ScriptletBatchService.groovy
@@ -196,7 +196,20 @@ class ScriptletBatchService implements ApplicationListener<ProcessingEvent> {
     }
 
     private List<Scriptlet> addScriptlets(ScriptletBatch batch, List runtimeAttributes) {
-        ScriptResolver scriptsResolver = new ScriptResolver(batch.executionZoneAction.scriptDir)
+        ScriptResolver scriptsResolver
+
+        try {
+            scriptsResolver = new ScriptResolver(batch.executionZoneAction.scriptDir)
+        }
+        catch (ProcessingException e) {
+            batch.state = Processable.ProcessState.FAILURE
+            batch.exceptionMessage = e.getMessage()
+            batch.exceptionStacktrace = e.getStackTrace()
+            batch.exceptionClass = e.class.getSimpleName()
+            batch.save(flush: true)
+            throw e
+        }
+
         PluginResolver pluginResolver = new PluginResolver(scriptDirectoryService.getPluginDir(batch.executionZoneAction.executionZone.type))
 
         scriptsResolver.resolve(runtimeAttributes).each { File file ->
