e7121e1610446bdf83aa135b1e14d3bedb02f653
Steiert, Daniel <daniel.steiert@sap.com>
2017-10-19 18:30:37 +0200

    RPI-2484 Make it possible to specify parameters on console

--- cmd/resttest.go
+++ cmd/resttest.go
@@ -5,15 +5,18 @@ import (
   "fmt"
 )
 
+var endpoint string
+
 func init() {
+  resttestCmd.Flags().StringVarP(&endpoint, "endpoint", "e", "hoststates", "the endpoint to GET from")
   RootCmd.AddCommand(resttestCmd)
 }
 
 var resttestCmd = &cobra.Command {
-  Use:   "resttest",
+  Use:   "resttest [endpoint]",
   Short: "Test a rest-command with zenboot-test",
   Run: func(cmd *cobra.Command, args []string) {
-      var content = sendRequest("GET", "help")
+      var content = sendRequest("GET", endpoint)
       fmt.Printf("%s\n", content)
   },
 }
