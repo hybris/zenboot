459d110402b0efc5bca22cf478e6810d5bc20665
Michael Stadelmeier <michael.stadelmeier@hybris.com>
2017-11-14 12:26:56 +0100

    Switch to correct stream.

--- grails-app/services/org/zenboot/portal/processing/ExecutionService.groovy
+++ grails-app/services/org/zenboot/portal/processing/ExecutionService.groovy
@@ -29,7 +29,7 @@ class ExecutionService {
               def process = data.execute()
               process.waitForOrKill(3600 * 5)
               process.errorStream.eachLine { groovyScript.scriptlet.processError.append(it + '\n')}
-              process.outputStream.eachLine { groovyScript.scriptlet.processOutput.append(it + '\n') }
+              process.inputStream.eachLine { groovyScript.scriptlet.processOutput.append(it + '\n') }
           }
           groovyScript.execute(ctx)
       } catch (Exception exc) {
