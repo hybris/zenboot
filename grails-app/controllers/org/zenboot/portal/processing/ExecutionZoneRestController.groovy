7ce94b038790dd4d010fb51f9640206820122cba
Michael Stadelmeier <michael.stadelmeier@hybris.com>
2017-11-21 14:43:03 +0100

    New REST endpoint added to get all service urls.

--- grails-app/controllers/org/zenboot/portal/processing/ExecutionZoneRestController.groovy
+++ grails-app/controllers/org/zenboot/portal/processing/ExecutionZoneRestController.groovy
@@ -19,6 +19,7 @@ import org.zenboot.portal.AbstractRestController
 import org.zenboot.portal.ControllerUtils
 import org.zenboot.portal.Host
 import org.zenboot.portal.HostState
+import org.zenboot.portal.ServiceUrl
 import org.zenboot.portal.security.Person
 import org.zenboot.portal.security.Role
 import org.zenboot.portal.Template
@@ -34,7 +35,7 @@ class ExecutionZoneRestController extends AbstractRestController implements Appl
 
     static allowedMethods = [index: "GET" , help: "GET", list: "GET", execute: "POST", listparams: "GET", listactions: "GET", createzone: "POST", exectypes: "GET", execzonetemplate: "GET",
     cloneexecutionzone: "GET", listhosts: "GET", listhoststates: "GET", changeExecutionZoneParams: ["POST", "DELETE"], changeExecutionZoneAttributes: ["POST"],
-    listDetailedActions: "GET"]
+    listDetailedActions: "GET", listServiceUrls: "GET"]
 
     @Override
     void setApplicationEventPublisher(ApplicationEventPublisher eventPublisher) {
@@ -496,12 +497,10 @@ class ExecutionZoneRestController extends AbstractRestController implements Appl
         }
     }
 
-
-        /**
+    /**
      * This method returns a list of all possible actions for the executionzone.
      */
     def listactions = {
-
         ExecutionZone executionZone
         File scriptDir
 
@@ -1307,7 +1306,6 @@ class ExecutionZoneRestController extends AbstractRestController implements Appl
             }
     }
 
-
     /**
      * The method returns a list of all existing states of a host.
      */
@@ -1633,7 +1631,7 @@ class ExecutionZoneRestController extends AbstractRestController implements Appl
                             if (SpringSecurityUtils.ifAllGranted(Role.ROLE_ADMIN) || userHasAccess(zone)) {
                                 execZones.add(zone)
                             } else {
-                                this.renderRestResult(HttpStatus.FORBIDDEN, null, null, 'You have no permissions to change the parameters ' +
+                                this.renderRestResult(HttpStatus.FORBIDDEN, null, null, 'You have no permissions to get the actions ' +
                                         'of the executionZone with id ' + params.execId + '.')
                                 return
                             }
@@ -1796,6 +1794,126 @@ class ExecutionZoneRestController extends AbstractRestController implements Appl
                     actionsJSON.put('executionZoneActions', actionsList)
                     render actionsJSON as JSON
                 }
+            }
+        }
+    }
+
+    /**
+     * The method return a list with active hosts service urls. It is possible to specify the execution zone or a list of execution zones delimited by ',' (?execId=1,2,3,5...). If the execId param is
+     * not set the method returns the service urls of all execution zones.
+     */
+    def listServiceUrls = {
+        def execZones = []
+        if (params.execId) {
+            if (params.execId.contains(',')) {
+                List<String> execZoneIds = params.execId.split(',')
+                execZoneIds.each {
+                    if(it.isInteger()) {
+                        ExecutionZone zone = ExecutionZone.get(it as Long)
+                        if (zone) {
+                            if (SpringSecurityUtils.ifAllGranted(Role.ROLE_ADMIN) || userHasAccess(zone)) {
+                                execZones.add(zone)
+                            } else {
+                                this.renderRestResult(HttpStatus.FORBIDDEN, null, null, 'You have no permissions to change the parameters ' +
+                                        'of the executionZone with id ' + params.execId + '.')
+                                return
+                            }
+                        }
+                    }
+                    else {
+                        this.renderRestResult(HttpStatus.BAD_REQUEST, null, null, 'The execId param ' + it +' invalid. ' +
+                                'It has to be a Long value.')
+                        return
+                    }
+                }
+            } else if (params.execId.isInteger()) {
+                ExecutionZone zone = ExecutionZone.get(params.execId as Long)
+                if (zone) {
+                    if (SpringSecurityUtils.ifAllGranted(Role.ROLE_ADMIN) || userHasAccess(zone)){
+                        execZones.add(zone)
+                    }
+                    else {
+                        this.renderRestResult(HttpStatus.FORBIDDEN, null, null, 'You have no permissions to get the serive urls ' +
+                                'of the executionZone with id ' + params.execId + '.')
+                        return
+                    }
+                }
+            } else {
+                this.renderRestResult(HttpStatus.BAD_REQUEST, null, null, 'The execId param invalid. ' +
+                        'It has to be a Long value or a list delimited by ","')
+                return
+            }
+        } else {
+            if (SpringSecurityUtils.ifAllGranted(Role.ROLE_ADMIN)) {
+                execZones = ExecutionZone.getAll()
+            } else {
+                def execZoneIds = accessService.accessCache[springSecurityService.getCurrentUserId() as Long]?
+                        accessService.accessCache[springSecurityService.getCurrentUserId() as Long].findAll {it.value == true}.collect {it.key}
+                        : []
+
+                execZoneIds.each {
+                    execZones.add(ExecutionZone.get(it as Long))
+                }
+            }
+        }
+
+        withFormat {
+            xml {
+                def builder = new StreamingMarkupBuilder()
+                builder.encoding = 'UTF-8'
+
+                String serviceURLsXML = builder.bind {
+                    serviceURLs {
+                        execZones.each { ExecutionZone zone ->
+                            zone.getActiveServiceUrls().each { ServiceUrl activeServiceUrl ->
+                                serviceURL {
+                                    id activeServiceUrl.id
+                                    url activeServiceUrl.url
+                                    serviceowner {
+                                        ipaddress activeServiceUrl.owner.ipAddress
+                                        cname activeServiceUrl.owner.cname
+                                        creationDate activeServiceUrl.owner.creationDate
+                                        expiryDate activeServiceUrl.owner.expiryDate
+                                        state activeServiceUrl.owner.state.name()
+                                        host {
+                                            id activeServiceUrl.owner.hostname.id
+                                            name activeServiceUrl.owner.hostname.name
+                                        }
+                                    }
+                                    execId zone.id
+                                }
+                            }
+                        }
+                    }
+                }
+
+                def xml = XmlUtil.serialize(serviceURLsXML).replace('<?xml version="1.0" encoding="UTF-8"?>', '<?xml version="1.0" encoding="UTF-8"?>\n')
+                xml = xml.replaceAll('<([^/]+?)/>', '<$1></$1>')
+                render contentType: "text/xml", xml
+            }
+            json {
+                def serviceURLsJSON = []
+                execZones.each { ExecutionZone zone ->
+                    zone.getActiveServiceUrls().each { ServiceUrl activeServiceUrl ->
+                        def serviceURLJSON = [:]
+                        serviceURLJSON.put('id', activeServiceUrl.id)
+                        serviceURLJSON.put('url', activeServiceUrl.url)
+                        def serviceOwner = [:]
+                        serviceOwner.put('ipaddress', activeServiceUrl.owner.ipAddress)
+                        serviceOwner.put('cname', activeServiceUrl.owner.cname)
+                        serviceOwner.put('creationDate', activeServiceUrl.owner.creationDate)
+                        serviceOwner.put('expiryDate', activeServiceUrl.owner.expiryDate)
+                        serviceOwner.put('state', activeServiceUrl.owner.state.name())
+                        def host = [:]
+                        host.put('id', activeServiceUrl.owner.hostname.id)
+                        host.put('name', activeServiceUrl.owner.hostname.name)
+                        serviceOwner.put('host', host)
+                        serviceURLJSON.put('serviceOwner', serviceOwner)
+                        serviceURLJSON.put('execId', zone.id)
+                        serviceURLsJSON.add(serviceURLJSON)
+                    }
+                }
+                render serviceURLsJSON as JSON
             }
         }
     }
