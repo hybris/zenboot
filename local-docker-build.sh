7ddd1924e72a6cea0f89cf1f62d87e3913446730
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2017-11-23 17:49:13 +0100

    RPI-2503 Remove separate 'go build' executions
    The CLI gets created when compiling zenboot using 'grails'. Hence skip
    invoking 'go build' when `grails compile` has already been run
    indirectly or directly.

--- local-docker-build.sh
+++ local-docker-build.sh
@@ -5,11 +5,4 @@ set -e
 ./grailsw -plain-output -non-interactive compile
 ./grailsw -plain-output -non-interactive war
 
-mkdir zenboot-cli/bin
-go get -d ./...
-GOOS=linux GOARCH=amd64 go build -o zenboot-cli/bin/zenboot-linux-amd64 zenboot-cli/zenboot.go
-GOOS=linux GOARCH=386 go build -o zenboot-cli/bin/zenboot-linux-386 zenboot-cli/zenboot.go
-GOOS=darwin GOARCH=amd64 go build -o zenboot-cli/bin/zenboot-darwin-amd64 zenboot-cli/zenboot.go
-GOOS=darwin GOARCH=386 go build -o zenboot-cli/bin/zenboot-darwin-386 zenboot-cli/zenboot.go
-
 docker build . -t hybris/zenboot --build-arg VERSION=ignored --build-arg ZENBOOT_WAR=target/zenboot.war --build-arg ZENBOOT_CLI=zenboot-cli/bin/zenboot-linux-amd64
