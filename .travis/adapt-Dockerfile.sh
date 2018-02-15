433f9c53a2b7b4a5cd46069754d9ed43e38b524d
Steiert, Daniel <daniel.steiert@sap.com>
2018-02-15 11:56:22 +0100

    Remove unneeded SSH keys for git commit during release

--- .travis/adapt-Dockerfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "# adapting Dockerfile"
-sed -i 's/ADD http.*/ADD https:\/\/github.com\/hybris\/zenboot\/releases\/download\/${TRAVIS_TAG}\/zenboot.war \/home\/user\/tomcat7\/webapps\/zenboot.war/' ../Dockerfile
-echo "# done, looks like this:"
-cat ./Dockerfile
-
